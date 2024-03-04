b9484d03e2088315ea9b1cdbeaa781d647609a53
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-03-04 15:06:10 -0500

    Fixed value to contain correct data about the breed instead of a numerical index.

--- script.js
+++ script.js
@@ -85,9 +85,10 @@ const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 const API_KEY =
   "live_Wav4bwy9xSzMtY5dxDWzZaZaI4PjIpyEQ2jGItCxW8D7WspoHRqCH8PyFdnhbmrc";
 
+  const url = `https://api.thecatapi.com/v1/breeds`;
+  let storedBreeds = [];
+
   async function initialLoad() {
-    const url = `https://api.thecatapi.com/v1/breeds`;
-    let storedBreeds = [];
   
     try {
       const response = await fetch(url, {
@@ -105,13 +106,16 @@ const API_KEY =
   
       for (let i = 0; i < storedBreeds.length; i++) {
         const breed = storedBreeds[i];
+        //console.dir(breed)
+        //console.log('is the breed')
         let option = document.createElement("option");
   
         // Skip any breeds that don't have an image
         if (!breed.image) continue;
   
         // Use the current array index
-        option.value = i;
+        option.value = `${breed.id}`;
+        //console.log(option.value)
         option.innerHTML = `${breed.name}`;
         document.getElementById("breedSelect").appendChild(option);
       }
@@ -121,30 +125,23 @@ const API_KEY =
       console.log(error);
     }
   
-//     function showBreedImage(index) {
-//       document.getElementById("breed_image").src = storedBreeds[index].image.url;
+    function showBreedImage(index) {
+      document.getElementById("breed_image").src = storedBreeds[index].image.url;
   
-//       document.getElementById("breed_json").textContent =
-//         storedBreeds[index].temperament;
+      document.getElementById("breed_json").textContent =
+        storedBreeds[index].temperament;
   
-//       document.getElementById("wiki_link").href =
-//         storedBreeds[index].wikipedia_url;
-//       document.getElementById("wiki_link").innerHTML =
-//         storedBreeds[index].wikipedia_url;
-//     }
+      document.getElementById("wiki_link").href =
+        storedBreeds[index].wikipedia_url;
+      document.getElementById("wiki_link").innerHTML =
+        storedBreeds[index].wikipedia_url;
+    }
  }
-  
   
 initialLoad();
 
-/**
- * 1. Create an async function "initialLoad" that does the following:
- * - Retrieve a list of breeds from the cat API using fetch().
- * - Create new <options> for each of these breeds, and append them to breedSelect.
- *  - Each option should have a value attribute equal to the id of the breed.
- *  - Each option should display text equal to the name of the breed.
- * This function should execute immediately.
- */
+
+
 
 /**
  * 2. Create an event handler for breedSelect that does the following:
