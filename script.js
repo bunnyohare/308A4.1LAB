5871e37d7b38f6e0adabce21d181698259971dd5
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-03-04 21:42:29 -0500

    Edited the commented out area to show I'd completed the second part of the lab.

--- script.js
+++ script.js
@@ -211,14 +211,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-/**
- * - Use the other data you have been given to create an informational section within the infoDump element.
- *  - Be creative with how you create DOM elements and HTML.
- *  - Feel free to edit index.html and styles.css to suit your needs, but be careful!
- *  - Remember that functionality comes first, but user experience and design are important.
- * - Each new selection should clear, re-populate, and restart the Carousel.
- * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
- */
 
 /**
  * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
